0b4ffc7c860c4b296be69a2f6fcdcd40f6e3704e
Matthew Aguirre <matthewaguirre@JSA-190134.local>
2019-03-06 14:18:22 -0800

    clears old code

--- Examples/js/earthquakeUSGS.js
+++ Examples/js/earthquakeUSGS.js
@@ -1,69 +1,5 @@
-// (function() {
-//     // Create the connector object
-//     var myConnector = tableau.makeConnector();
-//
-//     // Define the schema
-//     myConnector.getSchema = function(schemaCallback) {
-//         var cols = [{
-//             id: "id",
-//             dataType: tableau.dataTypeEnum.string
-//         }, {
-//             id: "mag",
-//             alias: "magnitude",
-//             dataType: tableau.dataTypeEnum.float
-//         }, {
-//             id: "title",
-//             alias: "title",
-//             dataType: tableau.dataTypeEnum.string
-//         }, {
-//             id: "location",
-//             dataType: tableau.dataTypeEnum.geometry
-//         }];
-//
-//         var tableSchema = {
-//             id: "earthquakeFeed",
-//             alias: "Earthquakes with magnitude greater than 4.5 in the last seven days",
-//             columns: cols
-//         };
-//
-//         schemaCallback([tableSchema]);
-//     };
-//
-//     // Download the data
-//     myConnector.getData = function(table, doneCallback) {
-//         $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
-//             var feat = resp.features,
-//                 tableData = [];
-//
-//             // Iterate over the JSON object
-//             for (var i = 0, len = feat.length; i < len; i++) {
-//                 tableData.push({
-//                     "id": feat[i].id,
-//                     "mag": feat[i].properties.mag,
-//                     "title": feat[i].properties.title,
-//                     "location": feat[i].geometry
-//                 });
-//             }
-//
-//             table.appendRows(tableData);
-//             doneCallback();
-//         });
-//     };
-//
-//     tableau.registerConnector(myConnector);
-//
-//     // Create event listeners for when the user submits the form
-//     $(document).ready(function() {
-//         $("#submitButton").click(function() {
-//             tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
-//             setTimeout(function(){tableau.submit()},3000); // This sends the connector object to Tableau
-//         });
-//     });
-// })();
-
 
 (function () {
-  //var excelToJson = require('convert-excel-to-json');
 
   var dataAsJson = [ { id: 0,
   start: 'Feb 25, 2019',
@@ -104,33 +40,6 @@
 
     var myConnector = tableau.makeConnector();
 
-  //   myConnector.getSchema = function (schemaCallback) {
-  //     console.log('getSchema Called')
-  //   var cols = [{
-  //       id: "id",
-  //       dataType: tableau.dataTypeEnum.string
-  //   }, {
-  //       id: "mag",
-  //       alias: "magnitude",
-  //       dataType: tableau.dataTypeEnum.float
-  //   }, {
-  //       id: "title",
-  //       alias: "title",
-  //       dataType: tableau.dataTypeEnum.string
-  //   }, {
-  //       id: "location",
-  //       dataType: tableau.dataTypeEnum.geometry
-  //   }];
-  //
-  //   var tableSchema = {
-  //       id: "earthquakeFeed",
-  //       alias: "Earthquakes with magnitude greater than 4.5 in the last seven days",
-  //       columns: cols
-  //   };
-  //
-  //   schemaCallback([tableSchema]);
-  // };
-
 
   myConnector.getSchema = function(schemaCallback) {
 
@@ -165,34 +74,7 @@
   }
 
 myConnector.getData = function(table, doneCallback) {
-  // $.getScript('excel-reader.js').done(function(script, textStatus) {
-  //   console.log('getting script')
-  //   let dat = getConversionData();
-  //   console.log(dat);
-  //
-  // }).fail(function(jqxhr, settings, exception) {
-  //   console.log('failure ', exception)
-  //   $( "div.log" ).text( "Triggered ajaxError handler." );
-  // })
 
-// $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
-//     console.log('getting json')
-//     var feat = resp.features,
-//         tableData = [];
-//
-//     // Iterate over the JSON object
-//     for (var i = 0, len = feat.length; i < len; i++) {
-//         tableData.push({
-//             "id": feat[i].id,
-//             "mag": feat[i].properties.mag,
-//             "title": feat[i].properties.title,
-//             "location": feat[i].geometry
-//         });
-//     }
-//
-//     table.appendRows(tableData);
-//     doneCallback();
-//     });
     var tableData = dataAsJson;
     // let tableData = getConversionData();
      table.appendRows(tableData);
@@ -205,7 +87,7 @@ myConnector.getData = function(table, doneCallback) {
 
 $(document).ready(function () {
     $("#submitButton").click(function () {
-        tableau.connectionName = "USGS Earthquake Feed";
+        tableau.connectionName = "JS Data Feed";
         tableau.submit();
     });
 });
