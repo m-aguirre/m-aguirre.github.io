1c272bacf65c2a0aedbada6b6b13e5fc88cd1283
Matthew Aguirre <matthewaguirre@JSA-190134.local>
2019-03-06 14:00:23 -0800

    places JSON object into function

--- Examples/js/earthquakeUSGS.js
+++ Examples/js/earthquakeUSGS.js
@@ -65,41 +65,46 @@
 (function () {
   //var excelToJson = require('convert-excel-to-json');
 
-    let dataAsJson = [ { id: 0,
-    start: 'Feb 25, 2019',
-    conversions: 15172,
-    end: 'Feb 25, 2019',
-    percent: 1 },
-  { id: 1,
-    start: 'Feb 26, 2019',
-    conversions: 10990,
-    end: 'Feb 26, 2019',
-    percent: 1 },
-  { id: 2,
-    start: 'Feb 27, 2019',
-    conversions: 9824,
-    end: 'Feb 27, 2019',
-    percent: 1 },
-  { id: 3,
-    start: 'Feb 28, 2019',
-    conversions: 10454,
-    end: 'Feb 28, 2019',
-    percent: 1 },
-  { id: 4,
-    start: 'Mar 1, 2019',
-    conversions: 12698,
-    end: 'Mar 1, 2019',
-    percent: 1 },
-  { id: 5,
-    start: 'Mar 2, 2019',
-    conversions: 12191,
-    end: 'Mar 2, 2019',
-    percent: 1 },
-  { id: 6,
-    start: 'Mar 3, 2019',
-    conversions: 13483,
-    end: 'Mar 3, 2019',
-    percent: 1 } ];
+    myConnector.getNikeData = function() {
+
+      let dataAsJson = [ { id: 0,
+        start: 'Feb 25, 2019',
+        conversions: 15172,
+        end: 'Feb 25, 2019',
+        percent: 1 },
+        { id: 1,
+          start: 'Feb 26, 2019',
+          conversions: 10990,
+          end: 'Feb 26, 2019',
+          percent: 1 },
+          { id: 2,
+            start: 'Feb 27, 2019',
+            conversions: 9824,
+            end: 'Feb 27, 2019',
+            percent: 1 },
+            { id: 3,
+              start: 'Feb 28, 2019',
+              conversions: 10454,
+              end: 'Feb 28, 2019',
+              percent: 1 },
+              { id: 4,
+                start: 'Mar 1, 2019',
+                conversions: 12698,
+                end: 'Mar 1, 2019',
+                percent: 1 },
+                { id: 5,
+                  start: 'Mar 2, 2019',
+                  conversions: 12191,
+                  end: 'Mar 2, 2019',
+                  percent: 1 },
+                  { id: 6,
+                    start: 'Mar 3, 2019',
+                    conversions: 13483,
+                    end: 'Mar 3, 2019',
+                    percent: 1 } ];
+
+      return dataAsJson;
+    }
 
     var myConnector = tableau.makeConnector();
 
@@ -164,7 +169,6 @@
   }
 
 myConnector.getData = function(table, doneCallback) {
-  console.log('getData called!')
   // $.getScript('excel-reader.js').done(function(script, textStatus) {
   //   console.log('getting script')
   //   let dat = getConversionData();
@@ -193,8 +197,7 @@ myConnector.getData = function(table, doneCallback) {
 //     table.appendRows(tableData);
 //     doneCallback();
 //     });
-    console.log('here')
-    let tableData = dataAsJson;
+    let tableData = myConnector.getNikeData();
     // let tableData = getConversionData();
      table.appendRows(tableData);
      console.log(tableData);
